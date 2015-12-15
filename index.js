39d50b59f54b76b9b1c20c4ec47c862c4f877ccd
blondprod <florian@exagroup.biz>
2015-12-16 00:46:38 +0100

    v0.3.1 build with object literral

--- index.js
+++ index.js
@@ -20,33 +20,37 @@ exports.handler = function(event, context) {
         var pdDateShippingWithFinish = "";
         var psPrintMethod = "";
         var psCommentBoard = "";
-        if(event.psAtelier != undefined)                    {psAtelier = event.psAtelier;}
-        if(event.pnIdPlanche != undefined)                  {pnIdPlanche = event.pnIdPlanche;}
-        if(event.psActiviteDeProd != undefined)             {psActiviteDeProd = event.psActiviteDeProd;}
-        if(event.pnPrice != undefined)                      {pnPrice = event.pnPrice;}
-        if(event.pnNbOrder != undefined)                    {pnNbOrder = event.pnNbOrder;}
-        if(event.psTitle != undefined)                      {psTitle = event.psTitle;}
-        if(event.pdDateCreation != undefined)               {pdDateCreation = event.psTitle;}
-        if(event.pdDateShippingWithoutFinish != undefined)  {pdDateShippingWithoutFinish = event.pdDateShippingWithoutFinish;}
-        if(event.pdDateShippingWithFinish != undefined)     {pdDateShippingWithFinish = event.pdDateShippingWithFinish;}
-        if(event.psPrintMethod != undefined)                {psPrintMethod = event.psPrintMethod;}
-        if(event.psCommentBoard != undefined)               {psCommentBoard = event.psCommentBoard;}
+        if(event.asBoard.psAtelier != undefined)                    {psAtelier = event.asBoard.psAtelier;}
+        if(event.asBoard.pnIdPlanche != undefined)                  {pnIdPlanche = event.asBoard.pnIdPlanche;}
+        if(event.asBoard.psActiviteDeProd != undefined)             {psActiviteDeProd = event.asBoard.psActiviteDeProd;}
+        if(event.asBoard.pnPrice != undefined)                      {pnPrice = event.asBoard.pnPrice;}
+        if(event.asBoard.pnNbOrder != undefined)                    {pnNbOrder = event.asBoard.pnNbOrder;}
+        if(event.asBoard.psTitle != undefined)                      {psTitle = event.asBoard.psTitle;}
+        if(event.asBoard.pdDateCreation != undefined)               {pdDateCreation = event.asBoard.psTitle;}
+        if(event.asBoard.pdDateShippingWithoutFinish != undefined)  {pdDateShippingWithoutFinish = event.asBoard.pdDateShippingWithoutFinish;}
+        if(event.asBoard.pdDateShippingWithFinish != undefined)     {pdDateShippingWithFinish = event.asBoard.pdDateShippingWithFinish;}
+        if(event.asBoard.psPrintMethod != undefined)                {psPrintMethod = event.asBoard.psPrintMethod;}
+        if(event.asBoard.psCommentBoard != undefined)               {psCommentBoard = event.asBoard.psCommentBoard;}
 
         //-- media
         var psMedia = "";
         var psSize = "";
         var pnQuantity = "";
-        if(event.psMedia != undefined)      {psMedia = event.psMedia;}
-        if(event.psSize != undefined)       {psSize = event.psSize;}
-        if(event.pnQuantity != undefined)   {pnQuantity = event.pnQuantity;}
+        if(event.asBoard.asMedia.psMedia != undefined)      {psMedia = event.asBoard.asMedia.psMedia;}
+        if(event.asBoard.asMedia.psSize != undefined)       {psSize = event.asBoard.asMedia.psSize;}
+        if(event.asBoard.asMedia.pnQuantity != undefined)   {pnQuantity = event.asBoard.asMedia.pnQuantity;}
 
         //-- operator
         var psOperatorName = "";
         var psOperatorSurName = "";
         var psOperatorMail = "";
-        if(event.psOperatorName != undefined)       {psOperatorName = event.psOperatorName;}
-        if(event.psOperatorSurName != undefined)    {psOperatorSurName = event.psOperatorSurName;}
-        if(event.psOperatorMail != undefined)       {psActiviteDeProd = event.psOperatorMail;}
+        if(event.asBoard.asOperator.psName != undefined)       {psOperatorName = event.asBoard.asOperator.psName;}
+        if(event.asBoard.asOperator.psSurName != undefined)    {psOperatorSurName = event.asBoard.asOperator.psSurName;}
+        if(event.asBoard.asOperator.psMail != undefined)       {psOperatorMail = event.asBoard.asOperator.psMail;}
+
+        //-- option
+        //var asSpec = "";
+        console.info(event.asBoard.asSpec[1].recto);
 
         //-- build XML
         var xml = builder.create('productionsheet',{version: '1.0', encoding: 'UTF-8'});
@@ -54,31 +58,29 @@ exports.handler = function(event, context) {
             //-- board
             xml = xml.com('board details').ele('board');
                 xml = getNode(xml,psAtelier,'workshop','workshop name/code');
-
-                xml = xml.com('board number').ele('idplanche');if(pnIdPlanche!=""){xml = xml.txt(pnIdPlanche);}xml = xml.up();
-                xml = xml.com('kind of production activity').ele('activity');if(psActiviteDeProd!=""){xml = xml.txt(psActiviteDeProd);}xml = xml.up();
-
-                xml = xml.com('board price').ele('price').txt(pnPrice).up();
-                xml = xml.com('number of orders').ele('nborder').txt(pnNbOrder).up();
-                xml = xml.com('board summary').ele('title').txt(psTitle).up();
-                xml = xml.com('creation date of the board').ele('datecreation').txt(pdDateCreation).up();
-                xml = xml.com('shipping date for order without finishing step').ele('shippingdatewithoutfinish').txt(pdDateShippingWithoutFinish).up();
-                xml = xml.com('shipping date for order with finishing step').ele('shippingdatewithfinish').txt(pdDateShippingWithFinish).up();
-                xml = xml.com('the kind of print method').ele('printmethod').txt(psPrintMethod).up();
-                xml = xml.com('technical comments').ele('commentboard').txt(psCommentBoard).up();
+                xml = getNode(xml,pnIdPlanche,'idplanche','board number');
+                xml = getNode(xml,psActiviteDeProd,'activity','kind of production activity');
+                xml = getNode(xml,pnPrice,'price','board price');
+                xml = getNode(xml,pnNbOrder,'nborder','number of orders');
+                xml = getNode(xml,psTitle,'title','board summary');
+                xml = getNode(xml,pdDateCreation,'datecreation','creation date of the board');
+                xml = getNode(xml,pdDateShippingWithoutFinish,'shippingdatewithoutfinish','shipping date for order without finishing step');
+                xml = getNode(xml,pdDateShippingWithFinish,'shippingdatewithfinish','shipping date for order with finishing step');
+                xml = getNode(xml,psPrintMethod,'printmethod','the kind of print method');
+                xml = getNode(xml,psCommentBoard,'commentboard','technical comments');
 
                 //-- media
                 xml = xml.com('media details').ele('media');
-                    xml = xml.com('media kind').ele('kind').txt(psMedia).up();
-                    xml = xml.com('size of media').ele('size').txt(psSize).up();
-                    xml = xml.com('media quantity').ele('quantity').txt(pnQuantity).up();
+                    xml = getNode(xml,psMedia,'kind','media kind');
+                    xml = getNode(xml,psSize,'size','size of media');
+                    xml = getNode(xml,pnQuantity,'quantity','media quantity');
                 xml = xml.up();
 
                 //-- operator
                 xml = xml.com('operator details').ele('operator');
-                    xml = xml.com('operator name').ele('name').txt(psOperatorName).up();
-                    xml = xml.com('operator surname').ele('surname').txt(psOperatorSurName).up();
-                    xml = xml.com('operator mail').ele('mail').txt(psOperatorMail).up();
+                    xml = getNode(xml,psOperatorName,'name','operator name');
+                    xml = getNode(xml,psOperatorSurName,'surname','operator surname');
+                    xml = getNode(xml,psOperatorMail,'mail','operator mail');
                 xml = xml.up();
 
             //-- end board
@@ -92,6 +94,7 @@ exports.handler = function(event, context) {
     }
     console.log("end\n");
 
+    // -- function to build node with value or not
     function getNode(xml,value,name,com)
     {
         if(value!=""){ return xml.com(com).ele(name).txt(value).up(); }
